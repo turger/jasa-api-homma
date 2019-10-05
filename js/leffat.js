bab88e8d641a31b9e2efb88e4b0293199195cfa8
Taru Airola <taru.airola@wunderdog.fi>
2019-10-06 00:22:41 +0300

    Lisää kysely elokuvien hakemiseen nimen perusteella

--- js/leffat.js
+++ js/leffat.js
@@ -1,3 +1,20 @@
 function nappula() {
   document.getElementById("juttu").innerHTML = "\"Maailmassa ei ole mitään mukavampaa kuin viihtyminen eikä mikään ole helpompaa.\" – Mymmeli"
 }
+
+function hae_elokuva() {
+  var haettu_leffa = document.getElementById("haettu_elokuva").value
+  // AJAX-kyselystä https://www.w3schools.com/js/js_ajax_http_send.asp
+  // AJAX readyState https://www.w3adda.com/ajax-tutorial/ajax-readystate
+  // HTTP-vastausten statuskoodit https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+  var xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var vastaus = JSON.parse(xhttp.responseText)
+      console.log('vastaus:', vastaus)
+      document.getElementById("elokuvan_nimi").innerHTML = vastaus.Title
+    }
+  }
+  xhttp.open("GET", "http://www.omdbapi.com/?apikey=a95f3723&t=" + haettu_leffa, true)
+  xhttp.send()
+}
